42ccde41cc99147107288479d5d7e871838fb6d8
Student <Student@IT-STULOAN-1751.csun.edu>
2022-10-10 19:18:27 -0700

    Commit-1 created entities

---
+++ ServiceReview/src/main/java/com/example/ServiceReview/Controller/Controller.java
@@ -0,0 +1,26 @@
+package com.example.ServiceReview.Controller;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ServiceReview.Entity.*;
+
+@RestController
+@CrossOrigin(origins="*")
+	public class Controller {
+
+		@GetMapping("Customer")
+		public List<Customer> getCustomerDetails(){
+			
+			return Stream.of(new Customer(1, "vaishnavi", "reddy", "vasihu@gmail.com", 1234),
+					new Customer(2, "surya", "gouthu", "surya@gmail.com", 2345),
+					new Customer(3, "mrudula", "reddy", "mrudula@gmail.com", 3456)).collect(Collectors.toList());
+			
+	}
+}
+
+
---
+++ ServiceReview/src/main/java/com/example/ServiceReview/Entity/Customer.java
@@ -0,0 +1,50 @@
+package com.example.ServiceReview.Entity;
+
+public class Customer {
+	private int id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private int phone;
+	public Customer(int id, String first_name, String last_name, String email, int phone) {
+		super();
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.phone = phone;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+
+}
